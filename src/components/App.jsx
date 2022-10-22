1c9a8a5057549ac100780adece7463894ab785fc
Raul Correa <raulcorrea0110@hotmail.com>
2022-10-22 11:34:21 -0500

    Commit inicial

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return (
+        <h1>Hola Amigos aqui se alojará la web</h1>
+    );
+}
+
+export default App;
