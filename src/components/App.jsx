32355f8fcb692ac7ee26a3407b9e038df3ebe9d1
Raul Correa <raulcorrea0110@hotmail.com>
2022-10-22 11:41:31 -0500

    se cambian de colores en prueba

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import '../styles/global.scss';
 
 const App = () => {
     return (
-        <h1>Hola Amigos aqui se alojará la web</h1>
+        <h1>Hola amigos aqui se alojará la web</h1>
     );
 }
 
